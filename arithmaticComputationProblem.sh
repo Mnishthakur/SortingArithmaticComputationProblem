35f21571c367a3a35118905a3547f5ad0d042d9c
“Mnishthakur” <mnish771@gmail.com>
2023-03-06 18:06:19 +0530

    Add:Compute a*b+c

--- arithmaticComputationProblem.sh
+++ arithmaticComputationProblem.sh
@@ -2,7 +2,7 @@ read -p "Enter a:" a
 read -p "Enter b:" b
 read -p "Enter c:" c
 
-result=$((a + b * c))
+result=$((a * b + c))
 
-echo "The result of a+b*c is : $result"
+echo "The result of a*b+c is : $result"
 
