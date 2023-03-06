e8da93738406451a3a1a98bef5f034f95aa42394
“Mnishthakur” <mnish771@gmail.com>
2023-03-06 18:43:00 +0530

    Add: Store the results in a Dictionary for every Computation

--- arithmaticComputationProblem.sh
+++ arithmaticComputationProblem.sh
@@ -1,7 +1,30 @@
-read -p "Enter a:" a
-read -p "Enter b:" b
-read -p "Enter c:" c
+# Initialize the dictionary
+declare -A results
 
+# Compute a + b * c and store the result in the dictionary
+a=2
+b=3
+c=4
+result=$((a + b * c))
+results["a + b * c"]=$result
+
+# Compute c + a / b and store the result in the dictionary
+a=6
+b=2
+c=4
+result=$((c + a / b))
+results["c + a / b"]=$result
+
+# Compute a % b + c and store the result in the dictionary
+a=11
+b=3
+c=4
 result=$((a % b + c))
+results["a % b + c"]=$result
 
-echo "The result of a % b + c is: $result"
+# Print the results in the dictionary
+echo "Results:"
+for key in "${!results[@]}"
+do
+  echo "$key = ${results[$key]}"
+done
